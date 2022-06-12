2687f48fa05e198111e5e64dc28fc351f0328be5
Amro Abdelkamel <amromoorie@gmail.com>
2022-06-12 07:48:08 +0000

    Sync LeetCode submission - Two Sum (javascript)

--- problems/two_sum/solution.js
+++ problems/two_sum/solution.js
@@ -3,11 +3,20 @@
  * @param {number} target
  * @return {number[]}
  */
+
 var twoSum = function (nums, target) {
+  const lookup = new Map()
   for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      let sum = nums[i] + nums[j]
-      if (sum === target) return [i, j]
+    // current iterating value
+    let current = nums[i]
+    // complementary value of current iterating value
+    let complementValue = target - current
+    // if lookup map has the complementValue;
+    // return the 2 indexes that adds up to target
+    if (lookup.has(complementValue)) {
+      return [i, lookup.get(complementValue)]
     }
+    // sets current iterating value for tracking
+    lookup.set(current, i)
   }
 }
