a2bcc488b7f628adb4639409ba4fbb76c395727f
Amro Abdelkamel <amromoorie@gmail.com>
2022-06-13 06:43:07 +0000

    Sync LeetCode submission - Reverse Integer (java)

--- problems/reverse_integer/solution.java
+++ problems/reverse_integer/solution.java
@@ -1,15 +1,16 @@
 class Solution {
-
     public int reverse(int x) {
+        int sign = x > 0 ? 1 : -1;
+        
         String numberStringified = Integer.toString(Math.abs(x));
         StringBuilder numberReversed = new StringBuilder(numberStringified).reverse();
+        String numberStringifiedReversed = numberReversed.toString();
+
         try {
-            int sign = x > 0 ? 1 : -1;
-            String numberStringifiedReversed = numberReversed.toString();
-           
             return Integer.parseInt(numberStringifiedReversed) * sign;
         } catch (NumberFormatException e) {
             return 0;
         }
+
     }
 }
