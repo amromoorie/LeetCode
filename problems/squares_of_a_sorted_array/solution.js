528223d8ae42e6e4b77b92227e80f8b980439792
Amro Abdelkamel <amromoorie@gmail.com>
2022-06-17 19:31:14 +0000

    Sync LeetCode submission - Squares of a Sorted Array (javascript)

--- problems/squares_of_a_sorted_array/solution.js
+++ problems/squares_of_a_sorted_array/solution.js
@@ -6,7 +6,7 @@ var sortedSquares = function (nums) {
   const n = nums.length
   const result = [].fill(n)
 
-  let index = n - 1
+  let pos = n - 1
   let start = 0
   let end = n - 1
 
@@ -15,13 +15,13 @@ var sortedSquares = function (nums) {
     let endValue = Math.pow(nums[end], 2)
 
     if (endValue >= startValue) {
-      result[index] = endValue
+      result[pos] = endValue
       end--
     } else {
-      result[index] = startValue
+      result[pos] = startValue
       start++
     }
-    index--
+    pos--
   }
   return result
 }
